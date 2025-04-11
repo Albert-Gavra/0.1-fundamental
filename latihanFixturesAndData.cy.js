287d2c454310fef92a2eb843571b60c8acb503dd
Albert-Gavra <albert.gavra@gmail.com>
2025-04-11 13:07:46 +0700

    latihan fixtures and data

---
+++ latihanFixturesAndData.cy.js
@@ -0,0 +1,32 @@
+/// <reference types="cypress" />\
+
+describe('Working with inputs', () => {
+    beforeEach(() => {
+        cy.visit('http://zero.webappsecurity.com/login.html')
+        cy.url().should('include', 'login.html')
+    });
+
+    it('Visit the website', () => {
+
+    });
+
+    it('should fill username and password', () => {
+        cy.fixture("user").then(user => {
+            const username = user.username
+            const password = user.password
+
+
+            cy.get('#user_login').clear()
+            cy.get('#user_login').type(username)
+
+            cy.get('input[name="user_password"]').clear()
+            cy.get('input[name="user_password"]').type(password)
+
+            cy.get('#user_remember_me').check()
+
+            cy.get('input[name="submit"]').click()
+
+            cy.get('.alert-error').should('contain.text', 'Login and/or password are wrong.')
+        })
+    });
+});
